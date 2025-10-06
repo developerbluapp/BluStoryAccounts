134c7b4ab1a165ef154b6f096810215d45045237
Amari <amari.lawal@gmail.com>
2025-10-06 17:59:51 +0100

    service account creation added

--- deployment/main.tf
+++ deployment/main.tf
@@ -18,31 +18,3 @@ module "service_accounts" {
   project_id = var.project_id
 }
 
-# ----------------------------
-# 1. Service Account + Secret module
-# ----------------------------
-module "secret_access" {
-  source          = "./modules/secret_access"
-  project_id      = var.project_id
-  key_file_path   =  var.key_file_path
-  service_account_key_json = module.service_accounts.service_account_key_json
-  secret_id       =  var.secret_id
-  depends_on     = [module.service_accounts]
-}
-
-
-
-# ----------------------------
-# 2. Cloud Run module
-# ----------------------------
-module "cloud_run" {
-  source                = "./modules/cloud_run"
-  project_id            = var.project_id
-  service_name          = var.service_name
-  region                = var.region
-  image                 = var.image
-  service_account_email = module.secret_access.cloud_run_sa_email
-  secret_id             = module.secret_access.secret_id
-  depends_on           = [module.secret_access, module.service_accounts]
-}
-
