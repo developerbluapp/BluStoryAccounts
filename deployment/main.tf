65793cb39407d8d6fe531091668a1917e483c031
Amari <amari.lawal@gmail.com>
2025-10-06 18:02:35 +0100

    service account creation added

--- deployment/main.tf
+++ deployment/main.tf
@@ -18,3 +18,17 @@ module "service_accounts" {
   project_id = var.project_id
 }
 
+# ----------------------------
+# 1. Service Account + Secret module
+# ----------------------------
+module "secret_access" {
+  source          = "./modules/secret_access"
+  project_id      = var.project_id
+  key_file_path   =  var.key_file_path
+  service_account_key_json = module.service_accounts.service_account_key_json
+  secret_id       =  var.secret_id
+  depends_on     = [module.service_accounts]
+}
+
+
+
