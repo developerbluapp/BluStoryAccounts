43995faa27f371a30d6619dbe801632febe53365
Amari <amari.lawal@gmail.com>
2026-03-03 21:16:01 +0000

    sign in works

--- models/responses/api/licenseholders/LicenseHolderResponse.py
+++ models/responses/api/licenseholders/LicenseHolderResponse.py
@@ -2,7 +2,6 @@
 from pydantic import BaseModel
 from uuid import UUID
 class LicenseHolderResponse(BaseModel):
-    model_config = {"arbitrary_types_allowed": True}
     id: UUID
     email: str
     username: str
